3f97ed15b7a65c697285d22c147b34056a7d9f5e
vilanmarius <48680778+vilanmarius@users.noreply.github.com>
2020-05-13 23:07:30 +0300

    Delete logintable.sql
    dublicate file

--- Megapuzzle/Puzzle_Vilan_Marius/Megapuzzle/Puzzle_Vilan_Marius/dbase/logintable.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Table] (
-    [loginID] INT           NOT NULL,
-    [username]    NVARCHAR (50) NULL,
-    [password]    NVARCHAR (50) NULL,
-    PRIMARY KEY CLUSTERED ([loginID] ASC)
-);
-
